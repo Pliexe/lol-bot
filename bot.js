647f81f4f2b5bc58da021dd757f3ab040d6dd093
Pliexe <42877326+Pliexe@users.noreply.github.com>
2019-07-10 04:23:09 +0200

    aawdaw

--- bot.js
+++ bot.js
@@ -1,3 +1,8 @@
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
+
 const {Client} = require('discord.js');
 
 const bot = new Client({disableEveryone: true});
